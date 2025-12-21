4eaa48cb9d257ef1eec5b84868b62fa90f85ad93
sagarde007 <sagarde2004@gmail.com>
2025-12-21 13:21:50 +0530

    ..

---
+++ _04_JavaScript/Day_03/index.js
@@ -0,0 +1,23 @@
+/* --------- >>>> memory management  <<<<----------------
+
+ static data (number - size fixed ) ----> stack 
+ dynamic data ( string - size not fixed) ---> heap
+
+
+ opetating system --> 
+     1 : 32 bit os --> 4 bit address size fix 
+     2 : 64 bit os --> 8 bit address size fix
+    
+    garbage collecter --> delete the garbage value  from heap 
+
+   
+
+*/
+    let counter = 10;                // Smi number
+    let price = 29.99;               // HeapNumber
+    let name = "Alice";                // String
+    let isActive = true;             // Boolean
+    let user = { id: counter };      // Object
+
+  
+
