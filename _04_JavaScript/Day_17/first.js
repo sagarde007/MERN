2300ef18e7435c0825d2284193cc21ca911ebe2d
sagarde007 <sagarde2004@gmail.com>
2026-01-22 14:17:24 +0530

    ..

---
+++ _04_JavaScript/Day_17/first.js
@@ -0,0 +1,72 @@
+/*
+---> Js is a single threaded synchronous language 
+(one task perfrom in one time)
+*/
+
+// Ex :- 01
+
+// console.log("First");
+// let sum = 0;
+// for(let i=0;i<3000000000;i++)
+//     sum+=i;
+
+// console.log(sum);
+// console.log("Last");
+
+
+// Ex :- 02  ()
+
+// console.log("Hello Ji");
+
+// setTimeout(() => {
+//     console.log("Time Out Executed");
+// }, 5000);
+
+// console.log("I am the end");
+
+
+
+// Ex :- 03
+
+// console.log("Hi Ji");
+
+// const button1 = document.getElementById("button1");
+// button1.addEventListener('click',()=>{
+//     console.log("Button 1 is clicked");
+// });
+
+
+// const button2 = document.getElementById("button2");
+// button2.addEventListener('click',()=>{
+//     console.log("Button 2 is clicked");
+// });
+
+// const button3 = document.getElementById("button3");
+// button3.addEventListener('click',()=>{
+//     console.log("Button 3 is clicked");
+// });
+
+// console.log("I am the end");
+
+
+// Ex :- 04  
+
+// console.log("I am first");
+
+// setTimeout(()=>{
+//     console.log("I am timeOut");
+// },5000);
+
+
+// console.log("I am end");
+
+
+// Ex :- 05   (Api call is a function call which execute a specific task)
+
+// console.log("Start the Operation");
+
+// fetch("https://api.github.com/users").then(() => {
+//   console.log("Git Hub user info");
+// });
+
+// console.log("end of operation");
