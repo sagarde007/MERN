caec1d39c0f8e510323078c45c50e8cbd564cb74
sagar De <sagarde2004@gmail.com>
2026-02-08 11:37:56 +0530

    ..

---
+++ _05_Git & GitHub/Day_02/first.js
@@ -0,0 +1,21 @@
+const name = "sagar de";
+const age = 22;
+const balance = 300;
+
+
+/*  
+        git log   // -----> showing all details
+        git show (id)  // ----> showing the details of edit by giving id
+*/
+
+
+/*
+        // create new branch
+
+        git chackout -b "name of branch"
+
+        git branch  // chack all the branch
+
+        // delete branch
+        git branch -D branch name
+*/
