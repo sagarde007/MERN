923958f10b293c4ab46715da51c311e5ed60033f
sagar De <sagarde2004@gmail.com>
2026-07-10 13:43:23 +0530

    Backend

---
+++ _08_BACKEND/Day_21/middleware/userAuth.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Token does not exist");
+    }
+
+    const payload = jwt.verify(req.cookies.token, "sagar@123");
+
+    const { _id } = payload;
+    if (!_id) {
+      throw new Error("ID is missing in token");
+    }
+
+    const result = await User.findById(_id);
+
+    if (!result) {
+      throw new Error("User not found");
+    }
+    req.result = result;
+
+    next();
+  } catch (err) {
+    res.send("Error" + err.message);
+  }
+};
+
+
+module.exports = userAuth;
